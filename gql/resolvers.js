0f9bfe8e2ac2f04c923210b8fd2797a17d64b1bb
BabsLabs <brian@brianbower.com>
2020-09-06 10:19:33 -0600

    Add queries for many to many relationships when quering a mushroom or berry by id

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -4,23 +4,44 @@ const database = require('knex')(configuration);
 
 const resolvers = {
   Query: {
+    
     mushrooms: async () => {
       return await database('mushrooms').select();
     },
+
     mushroom: async (parent, args) => {
-      return await database('mushrooms').where(args).first()
+      const mushroom =  await database('mushrooms').where(args).first()
+      const locationsOfASingleMushroom = await database('locations').whereIn('id',
+        database('mushroom_locations').select('location_id').where('mushroom_id', args.id)
+      )
+      mushroom.locations = locationsOfASingleMushroom
+      return mushroom
     },
+
     berries: async () => {
       return await database('berries').select()
     },
+
     berry: async (parent, args) => {
-      return await database('berries').where(args).first()
+      const berry = await database('berries').where(args).first()
+      const locationsOfASingleBerry = await database('locations').whereIn('id',
+        database('berry_locations').select('location_id').where('berry_id', args.id)
+      )
+      berry.locations = locationsOfASingleBerry
+      return berry
     },
+
     locations: async () => {
       return await database('locations').select()
     },
+
     location: async (parent, args) => {
-      return await database('locations').where(args).first()
+      const mushroomsOfASingleLocation = await database('mushrooms').whereIn('id',
+      database('mushroom_locations').select('mushroom_id').where('location_id', args.id)
+      )
+      const location = await database('locations').where(args).first()
+      location.mushrooms = mushroomsOfASingleLocation
+      return location
     },
 
     authenticationError: (parent, args, context) => {

4e020706f0085960660e03fae08a394bc52fd918
BabsLabs <brian@brianbower.com>
2020-09-07 12:16:36 -0600

    Add many to many relationships to locations query

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -9,8 +9,7 @@ const resolvers = {
       const mushrooms = await database('mushrooms').select();
       mushrooms.map( async (mushroom) => {
         const locationsOfASingleMushroom = database('locations').whereIn('id',
-          database('mushroom_locations').select('location_id').where('mushroom_id', mushroom.id)
-        )
+          database('mushroom_locations').select('location_id').where('mushroom_id', mushroom.id))
         mushroom.locations = locationsOfASingleMushroom
       })
       return mushrooms
@@ -19,8 +18,7 @@ const resolvers = {
     mushroom: async (parent, args) => {
       const mushroom =  await database('mushrooms').where(args).first()
       const locationsOfASingleMushroom = await database('locations').whereIn('id',
-        database('mushroom_locations').select('location_id').where('mushroom_id', args.id)
-      )
+        database('mushroom_locations').select('location_id').where('mushroom_id', args.id))
       mushroom.locations = locationsOfASingleMushroom
       return mushroom
     },
@@ -30,8 +28,7 @@ const resolvers = {
 
       berries.map(async (berry) => {
         const locationsOfASingleBerry = database('locations').whereIn('id',
-          database('berry_locations').select('location_id').where('berry_id', berry.id)
-        )
+          database('berry_locations').select('location_id').where('berry_id', berry.id))
         berry.locations = locationsOfASingleBerry
       })
       return berries
@@ -40,20 +37,32 @@ const resolvers = {
     berry: async (parent, args) => {
       const berry = await database('berries').where(args).first()
       const locationsOfASingleBerry = await database('locations').whereIn('id',
-        database('berry_locations').select('location_id').where('berry_id', args.id)
-      )
+        database('berry_locations').select('location_id').where('berry_id', args.id))
       berry.locations = locationsOfASingleBerry
       return berry
     },
 
     locations: async () => {
-      return await database('locations').select()
+      const locations = await database('locations').select()
+
+      locations.map((location) => {
+        const mushroomsOfASingleLocation = database('mushrooms').whereIn('id',
+          database('mushroom_locations').select('mushroom_id').where('location_id', location.id))
+      location.mushrooms = mushroomsOfASingleLocation
+      })
+
+      locations.map((location) => {
+        const berriesOfASingleLocation = database('berries').whereIn('id',
+          database('berry_locations').select('berry_id').where('location_id', location.id))
+      location.berries = berriesOfASingleLocation
+      })
+
+      return locations
     },
 
     location: async (parent, args) => {
       const mushroomsOfASingleLocation = await database('mushrooms').whereIn('id',
-      database('mushroom_locations').select('mushroom_id').where('location_id', args.id)
-      )
+      database('mushroom_locations').select('mushroom_id').where('location_id', args.id))
       const location = await database('locations').where(args).first()
       location.mushrooms = mushroomsOfASingleLocation
       return location
